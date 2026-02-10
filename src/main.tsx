ab758e75a6494da5b6b59f36c6fb664ef55f93cc
Sk26102001 <sushantkumar617@gmail.com>
2026-02-10 20:11:54 +0530

    bug fix

--- src/main.tsx
+++ src/main.tsx
@@ -6,12 +6,12 @@
 
 
 import { createRoot } from "react-dom/client";
-import {BrowserRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
-  <BrowserRouter>
+  <BrowserRouter basename="/auditclue-portal">
     <App />
   </BrowserRouter>
 );
